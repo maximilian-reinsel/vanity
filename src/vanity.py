b4b36eb43fcc6d19e6f347022534d6bba0a97b7a
David Torosyan <davidtorosyan.git@gmail.com>
2021-12-05 23:16:32 -0800

    Add --dmv-test option to query CA DMV

--- src/vanity.py
+++ src/vanity.py
@@ -1,6 +1,7 @@
 import argparse
 
 import license_explorer
+from dmv import CA_DMV
 
 parser = argparse.ArgumentParser(description="Searches for vanity license plates.")
 parser.add_argument("input", 
@@ -11,6 +12,13 @@ parser.add_argument("-w", "--output-width", type=int, default=10,
                     help="The maximum number of output per line in the output (10 by default)")
 parser.add_argument("-m", "--max-length", type=int, default=7, 
                     help="The maximum length of the outputs (7 by default)")
-
+parser.add_argument("--dmv-test", dest="dmv_test", action="store_true",
+                    help="if used, check the input against the CA DMV registry")
 args = parser.parse_args()
-license_explorer.explore(args.input, args.distance, args.output_width, args.max_length)
+
+if (args.dmv_test):
+    print("Is \"{}\" available as a CA DMV license plate?...".format(args.input))
+    dmv = CA_DMV()
+    print(dmv.checkPlate(args.input))
+else:
+    license_explorer.explore(args.input, args.distance, args.output_width, args.max_length)
